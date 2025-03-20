95f4a233e386cd310dc7ffda78296c4323655bdc
Josha Inglis <josha@jci.dev>
2025-03-20 17:36:48 +1100

    devenv and uv

---
+++ devenv.nix
@@ -0,0 +1,74 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+let
+  postgresql = pkgs.postgresql_16;
+  python = pkgs.python312;
+  #  sqlbag = python.pkgs.callPackage ./nix/dependencies/sqlbag.nix { };
+  #  schemainspect = python.pkgs.callPackage ./nix/dependencies/schemainspect.nix { inherit sqlbag; };
+  #  migra = python.pkgs.callPackage ./package.nix { inherit sqlbag schemainspect; };
+
+in
+{
+  packages = [
+    pkgs.git
+    postgresql
+    python
+    #    migra
+    config.languages.python.package.pkgs.psycopg2
+    config.languages.python.package.pkgs.sqlalchemy
+    config.languages.python.package.pkgs.pytest
+    config.languages.python.package.pkgs.pytest-cov
+    config.languages.python.package.pkgs.packaging
+    config.languages.python.package.pkgs.hatchling
+    config.languages.python.package.pkgs.uv
+  ];
+
+  languages.python = {
+    enable = true;
+    package = python;
+    uv.enable = true;
+    venv.enable = true;
+  };
+
+  services.postgres.enable = true;
+  services.postgres.package = postgresql;
+  services.postgres.listen_addresses = "127.0.0.1";
+  services.postgres.initialScript = "CREATE ROLE postgres SUPERUSER;";
+
+  enterShell = ''
+    git --version
+  '';
+
+  scripts.reset.exec = ''
+    rm -rf $DEVENV_STATE/postgres
+  '';
+  process.manager.before = ''reset'';
+
+  enterTest =
+    let
+      pg_isready = lib.getExe' config.services.postgres.package "pg_isready";
+    in
+    ''
+      echo "Running tests"
+      timeout 30 bash -c "until ${pg_isready} -d template1 -q; do sleep 0.5; done"
+      python -m pytest tests
+    '';
+
+  git-hooks.hooks = {
+    actionlint.enable = true;
+    ruff.enable = true;
+    ruff-format.enable = true;
+    check-toml.enable = true;
+    deadnix.enable = true;
+    nixfmt-rfc-style.enable = true;
+    end-of-file-fixer.enable = true;
+    markdownlint.enable = true;
+    pyupgrade.enable = true;
+    ripsecrets.enable = true;
+    trufflehog.enable = true;
+  };
+}
---
+++ package.nix
@@ -0,0 +1,65 @@
+{
+  lib,
+  buildPythonPackage,
+  psycopg2,
+  sqlalchemy,
+  sqlbag,
+  schemainspect,
+  networkx,
+  packaging,
+  hatchling,
+  pytestCheckHook,
+  pytest,
+  pytest-cov,
+  postgresql,
+  postgresqlTestHook,
+}:
+buildPythonPackage {
+  pname = "migra";
+  version = "4.0.0";
+  format = "pyproject";
+
+  doCheck = false;
+
+  src = ./.;
+
+  nativeBuildInputs = [ hatchling ];
+
+  propagatedBuildInputs = [
+    sqlalchemy
+    packaging
+    sqlbag
+    schemainspect
+    networkx
+    psycopg2
+  ];
+
+  nativeCheckInputs = [
+    pytestCheckHook
+
+    pytest
+    pytest-cov
+
+    postgresql
+    postgresqlTestHook
+  ];
+
+  preCheck = ''
+    export postgresqlTestUserOptions="LOGIN SUPERUSER"
+  '';
+
+  pytestFlagsArray = [
+    "-x"
+    "-svv"
+    "tests"
+  ];
+
+  pythonImportsCheck = [ "sqlbag" ];
+
+  meta = with lib; {
+    description = "Handy python code for doing database things";
+    homepage = "https://github.com/djrobstep/schemainspect";
+    license = with licenses; [ unlicense ];
+    maintainers = with maintainers; [ bpeetz ];
+  };
+}
